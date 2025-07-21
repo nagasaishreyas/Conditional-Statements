a3662e0b8167c7e94de72869b59291c424ccfe21
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-07-21 20:28:16 +0530

    sprt

---
+++ main2.py
@@ -0,0 +1,9 @@
+num = float (input("Enter a number: "))
+
+if num >= 0:
+    sprt = num ** 0.5
+    print(f"Square root of {num} is {sprt}")
+    
+else:
+    print("Cannot compute the square root of negative number: ")
+   
